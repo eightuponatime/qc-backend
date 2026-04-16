c9dbd642e606cabe088fd7249097f6b93eb1276f
maxim satanov <satanov201354@gmail.com>
2026-04-16 16:59:55 +0500

    feat: impl db settings and cors

--- cmd/main.go
+++ cmd/main.go
@@ -7,6 +7,10 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	chiMiddleware "github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
 )
 
 func main() {
@@ -15,6 +19,13 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to load config: %v", err)
 	}
+
+	db, err := sqlx.Connect("postgres", cfg.DatabaseURL)
+	if err != nil {
+		log.Fatalf("failed to connect to database %v", err)
+	}
+	defer db.Close()
+	log.Println("connected to database")
 
 	err = loadManifest()
 	if err != nil {
@@ -26,13 +37,20 @@ func main() {
 	r := chi.NewRouter()
 	r.Use(chiMiddleware.Logger)
 	r.Use(chiMiddleware.Recoverer)
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"https://*", "http://*"},
+		AllowedHeaders: []string{"Origin", "Content-Type", "Authorization"},
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "PATCH"},
+	}))
+
+	r.Handle("/static/*", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		err := tmpl.ExecuteTemplate(w, "index.html", map[string]any{
+		err := tmpl.ExecuteTemplate(w, "base.html", map[string]any{
 			"Title": "Home",
 		})
 		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError) 
+			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	})
 
